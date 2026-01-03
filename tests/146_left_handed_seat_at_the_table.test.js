9855f082cdd1287cb4bce4725b926e32bcf39a67
Johannes Werner <johannes.werner@cogitars.com>
2026-01-03 21:26:34 +0100

    Reformat test assertions for improved readability in "Left-Handed Seat at the Table" tests

--- tests/146_left_handed_seat_at_the_table.test.js
+++ tests/146_left_handed_seat_at_the_table.test.js
@@ -3,35 +3,50 @@ import { findLeftHandedSeats } from "../challenges/id_146_left_handed_seat_at_th
 
 Deno.test("find left-handed seats - test 1", () => {
   assertEquals(
-    findLeftHandedSeats([["U", "R", "U", "L"], ["U", "R", "R", "R"]]),
-    2
+    findLeftHandedSeats([
+      ["U", "R", "U", "L"],
+      ["U", "R", "R", "R"],
+    ]),
+    2,
   );
 });
 
 Deno.test("find left-handed seats - test 2", () => {
-    assertEquals(
-        findLeftHandedSeats([["U", "U", "U", "U"], ["U", "U", "U", "U"]]),
-        8
-    );
+  assertEquals(
+    findLeftHandedSeats([
+      ["U", "U", "U", "U"],
+      ["U", "U", "U", "U"],
+    ]),
+    8,
+  );
 });
 
 Deno.test("find left-handed seats - test 3", () => {
-    assertEquals(
-        findLeftHandedSeats([["U", "R", "U", "R"], ["L", "R", "R", "U"]]),
-        0
-    );
+  assertEquals(
+    findLeftHandedSeats([
+      ["U", "R", "U", "R"],
+      ["L", "R", "R", "U"],
+    ]),
+    0,
+  );
 });
 
 Deno.test("find left-handed seats - test 4", () => {
-    assertEquals(
-        findLeftHandedSeats([["L", "U", "R", "R"], ["L", "U", "R", "R"]]),
-        1
-    );
+  assertEquals(
+    findLeftHandedSeats([
+      ["L", "U", "R", "R"],
+      ["L", "U", "R", "R"],
+    ]),
+    1,
+  );
 });
 
 Deno.test("find left-handed seats - test 5", () => {
-    assertEquals(
-        findLeftHandedSeats([["U", "R", "U", "U"], ["U", "U", "L", "U"]]),
-        5
-    );
+  assertEquals(
+    findLeftHandedSeats([
+      ["U", "R", "U", "U"],
+      ["U", "U", "L", "U"],
+    ]),
+    5,
+  );
 });
